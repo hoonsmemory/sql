e74827cf4d71676e0c2c51ac8aa9166eaee901a1
BIT <BIT@DESKTOP-08BCIT0>
2019-09-03 19:25:57 +0900

    practice query 01

---
+++ pracetice/practice.sql
@@ -0,0 +1,90 @@
+-- practice01
+
+-- 문제1
+-- 사번이 10944인 사원의 이름은(전체 이름)
+select concat( first_name, ' ', last_name ) AS '전체 이름'
+  from employees 
+ where emp_no = 10944;
+
+-- 문제2
+-- 전체직원의 다음 정보를 조회하세요. 가장 선임부터 출력이 되도록 하세요. 
+-- 출력은 이름, 성별,  입사일 순서이고 “이름”, “성별”, “입사일로 컬럼 이름을 대체해 보세요.
+ select concat( emp.first_name, ' ', emp.last_name ) AS '전체 이름'
+	  , emp.gender                                   AS '성별'
+      , emp.hire_date                                AS '입사일'
+   from employees emp
+      , titles    ttl
+  where emp.emp_no  = ttl.emp_no
+    and ttl.to_date = '9999-01-01' 
+  order by emp.hire_date asc;
+
+-- 문제3
+-- 여직원과 남직원은 각 각 몇 명이나 있나요?
+ select (select count(emp1.gender)
+           from employees emp1
+		  where gender = 'F'
+            and emp1.gender = emp3.gender) as '여자 수'
+	  , (select count(emp2.gender)
+           from employees emp2
+		  where gender = 'M'
+           and emp2.gender = emp3.gender) as '남자 수'
+   from employees emp3
+  group by gender;
+
+ select emp2.gender as '성별'
+	  , (select count(*)
+           from employees emp1
+		  where emp1.gender = emp2.gender)as '총인원'
+   from employees emp2 
+  group by gender;
+  
+-- 문제4
+-- 현재 근무하고 있는 직원 수는 몇 명입니까? (salaries 테이블을 사용합니다.) 
+select count(distinct(emp_no)) as '현재 근무하고 있는 직원 수'
+  from salaries;
+
+-- 문제5
+-- 부서는 총 몇 개가 있나요?
+select count(dept_no) as '부서 총 개수'
+  from departments;
+
+-- 문제6
+-- 현재 부서 매니저는 몇 명이나 있나요?(역임 매너저는 제외)
+select count(distinct(emp_no)) as '현재 부서 매니저'
+  from dept_manager
+ where to_date = '9999-01-01';
+  
+-- 문제7
+-- 전체 부서를 출력하려고 합니다. 순서는 부서이름이 긴 순서대로 출력해 보세요.
+select *
+  from departments
+ order by length(dept_name) desc;
+  
+-- 문제8
+-- 현재 급여가 120,000이상 받는 사원은 몇 명이나 있습니까?
+select count(sal.emp_no) as '현재 급여가 120,000이상 받는 사원의 수'
+  from salaries sal
+ where sal.emp_no
+   and sal.salary >= 120000
+   and sal.to_date = '9999-01-01';
+
+-- 문제9
+-- 어떤 직책들이 있나요? 중복 없이 이름이 긴 순서대로 출력해 보세요.
+select distinct(title) as '직책'
+  from titles
+ order by length(title) desc;
+
+-- 문제10
+-- 현재 Engineer 직책의 사원은 총 몇 명입니까?
+select count(emp_no) as '직책의 사원수'
+  from titles
+ where title = 'Engineer'
+   and to_date = '9999-01-01';
+
+-- 문제11
+-- 사번이 13250(Zeydy)인 지원이 직책 변경 상황을 시간순으로 출력해보세요.
+select *
+  from titles
+ where emp_no = 13250
+ order by from_date;
+
--- select/basic.sql
+++ select/basic.sql
@@ -1,8 +1,111 @@
 -- select 기본
-(select emp_no from employees) from employees;
-
+-- employees 테이블에서 직원의 이름,  성별, 입사일을 출력
+select first_name
+     , last_name
+     , gender
+     , hire_date
+  from employees;
 
 -- concat
+-- employees 테이블에서 직원의 전체이름,  성별, 입사일을 출력
+select concat (first_name, ' ', last_name) 
+     , gender
+     , hire_date
+  from employees;
+
+-- alias -> as
+-- 생략 가능
+-- employees 테이블에서 직원의 이름,  성별, 입사일을 출력
+select concat (first_name, ' ', last_name) as 이름
+     , gender as 성별
+     ,  hire_date as '입사 날짜'
+  from employees;
+
+-- 중복 제거 distinct()
+-- select title from titles;
+select distinct(title) from titles;
+  
+-- order by
+-- employees 테이블에서 직원의 전체이름,  성별, 입사일을  입사일 순으로 출력
+select concat (first_name, ' ', last_name) as 이름
+     , gender as 성별
+     ,  hire_date as '입사 날짜'
+  from employees
+ order by hire_date desc;
+ 
+ -- salaries 테이블에서 2001년 월급을 가장 높은순으로 사번,  월급순으로 출력
+  SELECT emp_no,salary
+	FROM salaries
+   WHERE from_date like '2001-%' 
+ORDER BY salary DESC;
+
+-- employees 테이블에서 1991년 이전에 입사한 직원의 이름, 성별, 입사일을 출력
+
+  SELECT concat( first_name, ' ', last_name ) AS 이름
+       , gender AS 성별
+       , hire_date AS 입사일
+    FROM employees
+   WHERE hire_date < '1991-01-01'     
+   order by hire_date desc;
+
+-- employees 테이블에서 1989년 이전에 입사한 여직원의 이름, 입사일을 출력
+  SELECT concat( first_name, ' ', last_name ) AS 이름
+       , gender AS 성별
+       , hire_date AS 입사일
+    FROM employees
+   WHERE gender='f'
+     AND hire_date < '1989-01-01'
+   order by hire_date desc;
+
+-- dept_emp 테이블에서 부서 번호가 d005나 d009에 속한 사원의 사번, 부서번호 출력
+ SELECT emp_no, dept_no    
+   FROM dept_emp
+  WHERE dept_no in( 'd005', 'd009' );
+  
+-- like 검색
+-- employees 테이블에서 1989년에 입사한 직원의 이름, 입사일을 출력
+ SELECT concat( first_name, ' ', last_name ) AS 이름,
+         hire_date AS 입사일
+    FROM employees
+   WHERE hire_date <= '1989-12-31'
+	 and hire_date >= '1989-01-01'
+   order by hire_date desc;
+
+   -- upper
+-- 1.자바 upperCase보다 DB의 uppper() 함수가 훨씬 빠르다.
+-- 2.웬만한 DB에서 문자열 처리뿐만 아니라 포맷팅 처리등을 다 해주고 
+-- 자바에서는 화면 출력만 해결한다.
+-- 3. 자바 코드가 간결해서 좋다.
+
+select upper('Seoul'), lcase('seoul');
+select upper(first_name) from employees;
+
+-- lower
+select lower('Seoul'), lower('SEOUL');
+
+-- substring
+select substring('Happy Day',3,2) as substr;
+
+select first_name, substring(hire_date,1, 4) as '입사년도'
+  from employees;
+
+-- lpad, rpad
+select lpad('1234', 10, '-'), rpad('1234', 10, '-');
+
+-- salaries 테이블에서 2001년 급여가 70000불 이하의 직원만 사번, 급여로 출력하되 급여는 10자리로 부족한 자리수는 *로 표시
+select emp_no 
+     , lpad(salary, 10, '*') as salary
+  from salaries
+ where from_date like '2001%'
+   and salary < 70000;   
+
+-- ltrim, rtrim, trim
+select concat('--------',ltrim('     h e l l o    '), '---') as ltrim
+     , concat('--------',rtrim('     h e l l o    '), '---') as rtrim
+     , concat('--------',trim('     h e l l o     '), '---') as trim
+     , concat('--------',trim(both 'x' from 'xxxxxxh e l l oxxxxxx'), '---') as trim2
+     , concat('--------',trim(leading 'x' from 'xxxxxxh e l l oxxxxxx'), '---') as trim3
+     , concat('--------',trim(trailing 'x' from 'xxxxxxh e l l oxxxxxx'), '---') as trim4;
 
 
--- alias
+ 
